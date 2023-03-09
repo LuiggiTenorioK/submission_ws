80f737f39e9b29f64928ce4a99058a3084f29fdd
Luiggi Tenorio Ku <luiggi.tenorio@pucp.pe>
2023-03-09 14:53:53 +0100

    prepare for setup

--- submission_ws/submission_lib/scripts/blast.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-date1=$(date +"%s")
-echo "Started " "$(date)"
-blastp $1
-date2=$(date +"%s")
-DIFF=$((date2-date1))
-echo "Duration: $((DIFF / 3600 )) hours $(((DIFF % 3600) / 60)) minutes $((DIFF % 60)) seconds"
